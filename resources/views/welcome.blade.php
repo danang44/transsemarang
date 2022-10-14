949582842da3c133ffeb0984744cf056ff003c9d
Johan <johanrahar@gmail.com>
2022-10-14 11:41:47 +0700

    fixed layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 <body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="72">
     <header>
         <nav id="mainNav" class="navbar navbar-light navbar-expand-lg shadow fixed-top"
-            style="background: linear-gradient(#1D2939, #1D2939),#1D2939;padding-top: 28px;padding-bottom: 28px; padding-right: 136px;padding-left: 136px;">
+            style="background: linear-gradient(#1D2939, #1D2939),#1D2939;padding-top: 28px;padding-bottom: 28px; padding-right: 136px;padding-left: 136px; height: 100px;">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="#home">
                     <span
@@ -85,7 +85,7 @@
     </header>
     {{-- Section 1 - Slider --}}
     <section
-        style="background: url('assets/img/Hero.png') center / cover no-repeat;position: relative;height:790px;padding-top: 120px;margin-top: 110px;">
+        style="background: url('assets/img/Hero.png') center / cover no-repeat;position: relative;height:600px;padding-top: 120px;margin-top: 100px;">
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-left text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center"
@@ -121,7 +121,7 @@
         </div>
     </section>
     {{-- Section 2 - News --}}
-    <section style="padding-top: 80px; padding-bottom: 80px;padding-left: 120px;padding-right: 120px;">
+    <section style="display: flex;flex-direction: row;align-items: center;padding-top: 80px; padding-bottom: 80px;padding-left: 120px;padding-right: 120px;align-content: center;justify-content: center;">
         <div class="row row-cols-1"
             style="display: flex;flex-direction: row;align-items: flex-start;width: 1200px;height: 832px;">
             <div class="col-md-4 themed-grid-col" style="width: 412px">
@@ -631,48 +631,41 @@
         </div> --}}
     </section>
     {{-- Section Footer --}}
-    <footer>
-        <div class="container py-4 py-lg-5">
-            <div class="row justify-content-center">
+    <footer class="text-white" style="background-color: #AF2330">
+        <div class="col" style="padding-top: 40px;padding-bottom: 40px;padding-left: 120px;padding-right: 120px;">
+            <div class="row text-lg-start d-flex flex-column item" style="display: flex;flex-direction: row;justify-content: space-between;align-items: flex-start;padding: 0px;width: 1200px;height: 48px;padding-bottom: 40px;">
+                <img src="assets/img/Logobar.png" alt="" style="display: flex;flex-direction: row;align-items: left;padding: 0px;width: 256.95px;height: 48px;">
+                <p class="" style="font-weight: 700;font-size: 24px;line-height: 34px;letter-spacing: 2px;">TRANS SEMARANG</p>
+            </div>
+            <div class="row" style="display: flex;flex-direction: row;align-items: flex-start;padding: 0px;gap: 80px;width: 1200px;height: 170px;">
+            </div>
+        </div>
+
+            {{-- <div class="row justify-content-center">
                 <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 class="fs-6">Services</h3>
-                    <ul class="list-unstyled">
-                        <li><a class="link-secondary" href="#">Web design</a></li>
+                    <img src="assets/img/Logobar.png" alt="" style="">
+                    <p class="" style="font-weight: 700;font-size: 24px;line-height: 34px;letter-spacing: 2px;">TRANS SEMARANG</p>
+                    <ul class="list-unstyled ">
+                        <li><a class="link-secondary text-white" href="#">Web design</a></li>
                         <li><a class="link-secondary" href="#">Development</a></li>
                         <li><a class="link-secondary" href="#">Hosting</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 class="fs-6">About</h3>
-                    <ul class="list-unstyled">
-                        <li><a class="link-secondary" href="#">Company</a></li>
-                        <li><a class="link-secondary" href="#">Team</a></li>
-                        <li><a class="link-secondary" href="#">Legacy</a></li>
-                    </ul>
                 </div>
                 <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 class="fs-6">Careers</h3>
-                    <ul class="list-unstyled">
-                        <li><a class="link-secondary" href="#">Job openings</a></li>
-                        <li><a class="link-secondary" href="#">Employee success</a></li>
-                        <li><a class="link-secondary" href="#">Benefits</a></li>
-                    </ul>
                 </div>
-                <div
-                    class="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
-                    <div class="fw-bold d-flex align-items-center mb-2"><span
-                            class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2"><svg
-                                xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                                viewBox="0 0 16 16" class="bi bi-bezier">
-                                <path fill-rule="evenodd"
-                                    d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z">
-                                </path>
-                                <path
-                                    d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z">
-                                </path>
-                            </svg></span><span>Brand</span></div>
-                    <p class="text-muted copyright">Sem eleifend donec molestie, integer quisque orci aliquam.
-                    </p>
+                <div class="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
+                    <div class="fw-bold d-flex align-items-center mb-2">
+                        <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier">
+                                <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
+                                <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+                            </svg>
+                        </span>
+                        <span>Brand</span>
+                    </div>
+                    <p class="text-muted copyright">Sem eleifend donec molestie, integer quisque orci aliquam.</p>
                 </div>
             </div>
             <hr>
@@ -696,10 +689,11 @@
                             <path
                                 d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z">
                             </path>
-                        </svg></li>
+                        </svg>
+                    </li>
                 </ul>
-            </div>
-        </div>
+            </div> --}}
+
     </footer>
     {{-- <script src="assets/js/jquery.min.js"></script> --}}
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
